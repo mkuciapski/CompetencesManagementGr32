53909bc91b719f0537025091bcd8b866d4ebeb53
Michał Kuciapski <m.kuciapski@gmail.com>
2025-03-13 13:00:17 +0100

    Ref initial

--- src/views/Competences.vue
+++ src/views/Competences.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { ref } from 'vue'
+
+//Model
+const headerTitle = ref('<i>Competences</i>') //Data z Options API
+
+setTimeout(() => {
+  headerTitle.value = '<b>Reactive</b>'
+}, 3000)
+// Metody
+</script>
 
 <template>
-  <h1>Competences</h1>
+  <h1><span v-html="headerTitle"></span></h1>
 </template>
 
 <style scoped></style>
