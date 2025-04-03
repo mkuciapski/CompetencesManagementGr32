ca4ad959ec900d5f97de9e9d89d3c7f36b4d0615
Michał Kuciapski <m.kuciapski@gmail.com>
2025-04-03 13:42:00 +0200

    data

---
+++ src/data/lectures.ts
@@ -0,0 +1,51 @@
+const lectures = ['Rest', 'Front-end', 'Sieci', 'Ang']
+
+const grades = []
+grades.push(5)
+
+const WaldemarMessi = `Argentyno! Ojczyzno moja! Ty jesteś jak zdrowie.
+Ile cię trzeba cenić, ten tylko się dowie,
+Kto cię stracił. Dziś piękność twą w całej ozdobie
+Widzę i opisuję, bo tęsknię po tobie.
+
+Opiewam czyn szlachetny wielkiego Lionela,
+Co pączków słodkich mnogość po kryjomu rozdziela.
+Messi, ów piłkarz z argentyńskiej ziemi,
+Dziś między ławkami grasuje cichymi
+Krokami, by na biurkach zostawić przysmaki.
+
+Rankiem, kiedy słońce ledwo się wznosi,
+A kampus jeszcze drzemie w porannej rosie,
+Lionel Waldemar Messi, w stroju eleganckim,
+Z pudełkiem pączków przemierza poprzez korytarze.
+Jego kroki tak lekkie jak na murawie zwody,
+Gdy mija ściany, gdzie wiszą wizerunki godne.
+
+Do pierwszej sali wchodzi, rozgląda się wkoło,
+Profesor przy tablicy kreśli wzory z mozołem,
+Odwrócony plecami, w naukach zatopiony,
+Nie widzi, jak Argentyńczyk, cichutko, powoli,
+Na biurku kładzie pączek z różanym nadzieniem,
+By później był odkryty z wielkim zadziwieniem.
+
+Tak sala po sali, wydział po wydziale,
+Messi rozdaje pączki profesorom wytrwale.
+Jednym zostawia z lukrem, drugim z czekoladą,
+Trzecim z konfiturą, co najlepszą radą
+na trudy dnia będzie i osłodą pracy,
+Gdy oni błądzą myślami gdzieś w obłokach inaczej.
+
+A gdy pytają głośno: „Kto nas tak rozpieszcza?
+Kto te słodkie przysmaki na biurkach nam zostawia,
+Gdy uwaga nasza gdzie indziej podąża?"
+Nikt im nie odpowie,
+Bo tajemnicę tę Messi chowa w swej głowie.
+Czasem tylko przemyka po trawach zielonych,
+Sylwetka jego smukła, wśród drzew osłoniona.
+
+I tak dzień po dniu, miesiące już trwała
+Pączków epopeja, która zachwycała
+Profesorów, gdy oni z wielkim zdumieniem
+Znajdowali słodkości z najlepszym nadzieniem.`
+
+export { lectures }
---
+++ src/data/students.ts
@@ -0,0 +1,4 @@
+export default [
+  { Name: 'Waldemar', LastName: 'Kiepski', Grade: 2 },
+  { Name: 'Waldemar', LastName: 'Messi', Grade: 2 },
+]
